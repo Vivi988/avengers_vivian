f98b652f3ab0b5508f0f6830ecf341c8c1744192
Vivian Uedre <vuedre@gmail.com>
2024-02-20 23:17:21 +1100

    Création des templates pour affichages des données livres

--- src/Controller/LivresController.php
+++ src/Controller/LivresController.php
@@ -5,14 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Livre;
 
 class LivresController extends AbstractController
 {
     #[Route('/livres', name: 'app_livres')]
-    public function index(): Response
+    public function getAll(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('livres/index.html.twig', [
-            'controller_name' => 'LivresController',
+        // Récupère les livres dans la BDD
+        $livres = $entityManager->getRepository(Livre::class)->findAll();
+        
+        // Gestion d'erreur
+        if(!$livres) {
+            throw $this->createNotFoundException("Aucun livre n'est enregistré !");
+        }
+
+        // Transfère les données à la Vue
+        return $this->render('livres/liste.html.twig', [
+            'livres' => $livres,
+        ]);
+    }
+
+    // Définition de la route 
+    #[Route('/consulter/{id}', requirements: ["id" => "\d+"])]
+    public function consulterDetails(int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Cherche les marques pages en bdd selon l'ID de l'article
+        $details = $entityManager->getRepository(Livre::class)->find($id);
+
+        if(!$details) {
+            throw $this->createNotFoundException(
+                "Aucun livre avec l'id ". $id
+            );
+        }
+
+        // Ajoute les valeurs dans la Vue
+        return $this->render('livres/details.html.twig', [
+            'details' => $details,
         ]);
     }
 }
