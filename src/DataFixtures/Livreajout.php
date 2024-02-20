88cb8783a7a699155b561e608ae773edd1cf0fc0
Vivian Uedre <vuedre@gmail.com>
2024-02-21 09:33:04 +1100

    AVdJ 6 - Charger 15 livres

---
+++ src/DataFixtures/Livreajout.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Livre;
+use DateTime;
+
+class Livreajout extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        // Création de 15 livres
+        for ($i = 0; $i < 15; $i++) {
+            $livre = new Livre();
+            $livre->setTitre('Livre ' . $i);
+            $livre->setAuteur('Auteur ' . $i);
+            $livre->setAnnee(new \DateTime(mt_rand(1975, 2020)));
+            $manager->persist($livre);
+        }
+        $manager->flush();
+    }
+}
