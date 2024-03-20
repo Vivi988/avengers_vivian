336de105d5dab4ab58b8a0f39f6cacadaf455b5b
Vivian Uedre <vuedre@gmail.com>
2024-03-21 08:20:36 +1100

    AVdJ 3/5 Cntrlr + Entite Adresse

---
+++ src/Entity/Adresse.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdresseRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdresseRepository::class)]
+class Adresse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Adresse = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Commune = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $text = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(?string $Adresse): static
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getCommune(): ?string
+    {
+        return $this->Commune;
+    }
+
+    public function setCommune(string $Commune): static
+    {
+        $this->Commune = $Commune;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+}
